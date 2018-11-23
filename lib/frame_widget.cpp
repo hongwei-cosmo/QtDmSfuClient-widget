21073f9b6a65cff895a8065772e352cfad7cae44
Hongwei Gao <hongwei.gao@cosmosoftware.io>
2018-11-23 15:56:51 +0800

    scale pixmap

--- lib/frame_widget.cpp
+++ lib/frame_widget.cpp
@@ -3,8 +3,5 @@
 void FrameWidget::drawImage(const QImage &image)
 {
   const QPixmap &pixmap = QPixmap::fromImage(image);
-//  if ((label->width() != image.width()) || (label->height() != image.height())) {
-//    label->resize(image.width(), image.height());
-//  }
-  setPixmap(pixmap);
+  setPixmap(pixmap.scaled(width(), height(), Qt::KeepAspectRatio));
 }
